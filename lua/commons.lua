86bd71829473d88c7461541df3169744c4b17ee9
linrongbin16 <linrongbin16@outlook.com>
2023-12-11 22:28:04 +0800

    chore

--- lua/commons.lua
+++ lua/commons.lua
@@ -6,8 +6,20 @@ local Defaults = {
 
 local Configs = {}
 
-M.setup = function()
-  local base_dir = vim.fn["commons#nvim#base_dir"]()
+--- @alias commons.Options table<any, any>
+--- @param opts commons.Options?
+M.setup = function(opts)
+  if
+    type(opts) == "table"
+    and type(opts.module_prefix) == "string"
+    and string.len(opts.module_prefix) > 0
+  then
+    local prefix = opts.module_prefix
+    if string.sub(prefix, #prefix) ~= "." then
+      prefix = prefix .. "."
+    end
+    vim.env._COMMONS_NVIM_MODULE_PREFIX = prefix
+  end
 end
 
 return M
