d00be8a1272bb644ecb2a82f0f6a230e2f522467
linrongbin16 <linrongbin16@outlook.com>
2023-12-13 10:08:26 +0800

    docs(page): use markdown docs (#35)

--- lua/commons/jsons.lua
+++ lua/commons/jsons.lua
@@ -1,10 +1,11 @@
 -- Json encode/decode
 
-local M = {
-  encode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.encode
-    or require("commons.actboy168_json").encode,
-  decode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.decode
-    or require("commons.actboy168_json").decode,
-}
+local M = {}
+
+M.encode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.encode
+  or require("commons.actboy168_json").encode
+
+M.decode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.decode
+  or require("commons.actboy168_json").decode
 
 return M
