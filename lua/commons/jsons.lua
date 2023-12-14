e7385bc9995652d09f8bb94bc0b45ca57f601af7
linrongbin16 <linrongbin16@outlook.com>
2023-12-14 14:59:20 +0800

    chore

--- lua/commons/jsons.lua
+++ lua/commons/jsons.lua
@@ -4,6 +4,9 @@ local M = {}
 --- @param t table?   lua table.
 --- @return string?   returns json string.
 M.encode = function(t)
+  if t == nil then
+    return nil
+  end
   if vim.fn.has("nvim-0.9") and vim.json ~= nil then
     return vim.json.encode(t)
   else
@@ -14,6 +17,9 @@ end
 --- @param j string?  json string.
 --- @return table?    returns lua table.
 M.decode = function(j)
+  if j == nil then
+    return nil
+  end
   if vim.fn.has("nvim-0.9") and vim.json ~= nil then
     return vim.json.decode(j)
   else
