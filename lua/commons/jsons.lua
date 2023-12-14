97f2ef5c5ffe169a654d21a4fc44d26fdd76f5cf
linrongbin16 <linrongbin16@outlook.com>
2023-12-14 14:55:50 +0800

    chore

--- lua/commons/jsons.lua
+++ lua/commons/jsons.lua
@@ -1,13 +1,13 @@
 ---@diagnostic disable: undefined-doc-param
 local M = {}
 
---- @param t table?   lua table.
---- @return string?   returns json string.
+--- @param t table    lua table.
+--- @return string    returns json string.
 M.encode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.encode
   or require("commons._json").encode
 
---- @param j string?  json string.
---- @return table?    returns lua table.
+--- @param j string   json string.
+--- @return table     returns lua table.
 M.decode = (vim.fn.has("nvim-0.9") and vim.json ~= nil) and vim.json.decode
   or require("commons._json").decode
 
