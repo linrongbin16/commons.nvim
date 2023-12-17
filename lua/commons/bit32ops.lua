b37838ac84fed028ad3c65ca5a30b1cba2176383
linrongbin16 <linrongbin16@outlook.com>
2023-12-17 12:33:30 +0800

    Update bit32ops.lua

--- lua/commons/bit32ops.lua
+++ lua/commons/bit32ops.lua
@@ -1,11 +1,6 @@
-local M = {}
-
-do
-  local _bitop = require("commons._bitop")
-
-  for k, v in pairs(_bitop) do
-    M[k] = v
-  end
+local ok, c_ext = pcall(require, "bit")
+if ok then
+  return c_ext
 end
-
-return M
+  
+return require("commons._bitop").bit
