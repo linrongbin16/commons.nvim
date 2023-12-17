6b508fe9d754471981f552b3809e6f28c5c383b3
linrongbin16 <linrongbin16@users.noreply.github.com>
2023-12-17 03:54:51 +0000

    chore(pr): auto-commit

--- lua/commons/bit32ops.lua
+++ lua/commons/bit32ops.lua
@@ -1,11 +1,11 @@
 local M = {}
 
-do 
-local _bitop = require("commons._bitop")
+do
+  local _bitop = require("commons._bitop")
 
-for k, v in pairs(_bitop) do
-  M[k] = v
-end
+  for k, v in pairs(_bitop) do
+    M[k] = v
+  end
 end
 
 return M
