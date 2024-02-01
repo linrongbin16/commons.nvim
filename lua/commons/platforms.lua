4a013334cfeb7ddff03fce34b9865f73963e3536
linrongbin16 <linrongbin16@outlook.com>
2024-02-02 01:50:41 +0800

    feat(platforms): add 'platforms' module

---
+++ lua/commons/platforms.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.IS_WINDOWS = vim.fn.has("win32") > 0 or vim.fn.has("win64") > 0
+M.IS_MAC = vim.fn.has("mac") > 0
+M.IS_BSD = vim.fn.has("bsd") > 0
+M.IS_LINUX = (vim.fn.has("linux") > 0 or vim.fn.has("unix") > 0)
+  and not M.IS_WINDOWS
+  and not M.IS_MAC
+  and not M.IS_BSD
