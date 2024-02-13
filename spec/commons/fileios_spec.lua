9ad8166a4e07084733f14e56f2d6562b57560e95
linrongbin16 <linrongbin16@outlook.com>
2024-02-13 15:54:35 +0800

    Update fileios_spec.lua

--- spec/commons/fileios_spec.lua
+++ spec/commons/fileios_spec.lua
@@ -16,7 +16,7 @@ describe("commons.fileios", function()
 
 describe("[FileLineReader]", function()
     it("failed to open", function()
-      local ok, reader = pcall(fileios.FileLineReader:open, "asdf.md") --[[@as commons.FileLineReader]]
+      local ok, reader = pcall(fileios.FileLineReader:open, "asdf.md")
       assert_eq(reader, nil)
       assert_false(ok)
     end)
