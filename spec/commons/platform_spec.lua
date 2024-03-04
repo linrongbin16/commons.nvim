f92727032bd939b4ff3e0634ef42d0fb511c1e50
linrongbin16 <linrongbin16@outlook.com>
2024-03-04 11:41:46 +0800

    chore

--- spec/commons/platform_spec.lua
+++ spec/commons/platform_spec.lua
@@ -1,6 +1,6 @@
 local cwd = vim.fn.getcwd()
 
-describe("commons.platforms", function()
+describe("commons.platform", function()
   local assert_eq = assert.is_equal
   local assert_true = assert.is_true
   local assert_false = assert.is_false
@@ -11,18 +11,18 @@ describe("commons.platforms", function()
     vim.api.nvim_command("cd " .. cwd)
   end)
 
-  local platforms = require("commons.platforms")
+  local platform = require("commons.platform")
   describe("[OS_NAME]", function()
     it("test", function()
-      assert_true(string.len(platforms.OS_NAME) > 0)
+      assert_true(string.len(platform.OS_NAME) > 0)
     end)
   end)
   describe("[IS_XXX]", function()
     it("test", function()
       if vim.fn.has("win32") > 0 or vim.fn.has("win64") > 0 then
-        assert_true(platforms.IS_WINDOWS)
+        assert_true(platform.IS_WINDOWS)
       else
-        assert_true(platforms.IS_LINUX or platforms.IS_MAC)
+        assert_true(platform.IS_LINUX or platform.IS_MAC)
       end
     end)
   end)
