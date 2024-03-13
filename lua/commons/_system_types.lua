98ce7abe8f33b8e646abd7eeb42f1c7222552736
linrongbin16 <linrongbin16@outlook.com>
2024-03-13 09:40:27 +0800

    chore

--- lua/commons/_system_types.lua
+++
@@ -1,8 +0,0 @@
----@diagnostic disable: undefined-doc-name
-
--- Fix typecheck for _system.lua
---- @alias uv.uv_process_t uv_process_t
---- @alias uv.uv_timer_t uv_timer_t
---- @alias uv.uv_stream_t uv_stream_t
---- @alias uv.uv_handle_t uv_handle_t
---- @alias uv.spawn.options table<any, any>
