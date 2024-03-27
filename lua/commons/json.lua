ca93a25774328475bfee6ca7652562bc3dc2067d
linrongbin16 <linrongbin16@outlook.com>
2024-03-27 10:01:06 +0800

    perf(json): use vim builtin lua modules

--- lua/commons/json.lua
+++ lua/commons/json.lua
@@ -1,12 +1,14 @@
 local M = {}
 
+local NVIM_090 = require("commons.version").ge("0.9")
+
 --- @param t table?
 --- @return string?
 M.encode = function(t)
   if t == nil then
     return nil
   end
-  return require("commons._json").encode(t)
+  return NVIM_090 and vim.json.encode(t) or require("commons._json").encode(t)
 end
 
 --- @param j string?
@@ -15,7 +17,7 @@ M.decode = function(j)
   if j == nil then
     return nil
   end
-  return require("commons._json").decode(j)
+  return NVIM_090 and vim.json.decode(j) or require("commons._json").decode(j)
 end
 
 return M
