4f5754f7cc3429731a312e228d41c995edae8669
linrongbin16 <linrongbin16@outlook.com>
2024-03-27 10:05:17 +0800

    chore

--- lua/commons/uri.lua
+++ lua/commons/uri.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local NVIM_010 = require("commons.version").ge("0.10")
-
 ---@param value string?
 ---@param rfc "rfc2396"|"rfc2732"|"rfc3986"|nil
 ---@return string?
@@ -9,7 +7,11 @@ M.encode = function(value, rfc)
   if type(value) ~= "string" then
     return nil
   end
-  return NVIM_010 and vim.uri_encode(value, rfc) or require("commons._uri").uri_encode(value, rfc)
+  if vim.is_callable(vim.uri_encode) then
+    return vim.uri_encode(value, rfc)
+  else
+    return require("commons._uri").uri_encode(value, rfc)
+  end
 end
 
 ---@param value string?
@@ -18,7 +20,11 @@ M.decode = function(value)
   if type(value) ~= "string" then
     return nil
   end
-  return NVIM_010 and vim.uri_decode(value) or require("commons._uri").uri_decode(value)
+  if vim.is_callable(vim.uri_decode) then
+    return vim.uri_decode(value)
+  else
+    return require("commons._uri").uri_decode(value)
+  end
 end
 
 return M
