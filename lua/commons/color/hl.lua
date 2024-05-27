f6ff52202fbbda860584af143cec90b092143e7b
linrongbin16 <linrongbin16@outlook.com>
2024-05-27 09:25:36 +0800

    chore

--- lua/commons/color/hl.lua
+++ lua/commons/color/hl.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+--- @param ... string?
+--- @return {fg:integer?,bg:integer?,[string]:any,ctermfg:integer?,ctermbg:integer?,cterm:{fg:integer?,bg:integer?,[string]:any}?}, integer, string?
+M.get_hl_with_fallback = function(...)
+  for i, hl in ipairs({ ... }) do
+    if type(hl) == "string" then
+      local hl_value = vim.api.nvim_get_hl(0, { name = hl, link = false })
+      if type(hl_value) == "table" and not vim.tbl_isempty(hl_value) then
+        return hl_value, i, hl
+      end
+    end
+  end
+
+  return vim.empty_dict(), -1, nil
+end
+
 --- @param highlight string
 --- @param attr "fg"|"bg"|string
 --- @return string?
