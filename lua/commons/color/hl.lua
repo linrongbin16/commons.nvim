2f109bc0b2b4288dd3294bbf4eca309357630115
linrongbin16 <linrongbin16@outlook.com>
2024-05-27 09:43:16 +0800

    chore

--- lua/commons/color/hl.lua
+++ lua/commons/color/hl.lua
@@ -1,11 +1,17 @@
 local M = {}
 
+--- @param hl string
+--- @return {fg:integer?,bg:integer?,[string]:any,ctermfg:integer?,ctermbg:integer?,cterm:{fg:integer?,bg:integer?,[string]:any}?}
+M.get_hl = function(hl)
+  return vim.api.nvim_get_hl(0, { name = hl, link = false })
+end
+
 --- @param ... string?
 --- @return {fg:integer?,bg:integer?,[string]:any,ctermfg:integer?,ctermbg:integer?,cterm:{fg:integer?,bg:integer?,[string]:any}?}, integer, string?
 M.get_hl_with_fallback = function(...)
   for i, hl in ipairs({ ... }) do
     if type(hl) == "string" then
-      local hl_value = vim.api.nvim_get_hl(0, { name = hl, link = false })
+      local hl_value = M.get_hl(hl)
       if type(hl_value) == "table" and not vim.tbl_isempty(hl_value) then
         return hl_value, i, hl
       end
@@ -22,8 +28,7 @@ M.get_color = function(highlight, attr)
   assert(type(highlight) == "string")
   assert(attr == "fg" or attr == "bg" or attr == "sp")
 
-  local api = require("commons.api")
-  local hl_value = vim.api.nvim_get_hl(0, { name = highlight, link = false })
+  local hl_value = M.get_hl(highlight)
   if type(hl_value) == "table" and type(hl_value[attr]) == "number" then
     return string.format("#%06x", hl_value[attr])
   end
