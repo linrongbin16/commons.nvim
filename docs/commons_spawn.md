7d1f0edc1e428f723e80c489c8fb4e1e5b09c8bc
linrongbin16 <linrongbin16@outlook.com>
2025-01-14 17:53:38 +0800

    feat(spawn)!: rename "run" to "linewise" for line-wise stdout/stderr processing (#299)
    feat(spawn): add "complete" for all collected stdout/stderr from child-process (#299)

--- docs/commons_spawn.md
+++ docs/commons_spawn.md
@@ -2,50 +2,163 @@
 
 # [commons.spawn](https://github.com/linrongbin16/commons.nvim/blob/main/lua/commons/spawn.lua)
 
-Run child-process with friendly line-wise callbacks to handle stdout/stderr output, a wrapper on [vim.system](<https://neovim.io/doc/user/lua.html#vim.system()>).
+Run child-process with both line-wise/until-complete callbacks to handle stdout/stderr output, a wrapper on [vim.system](<https://neovim.io/doc/user/lua.html#vim.system()>).
 
 ## Functions
 
-### `run`
+### `complete`
 
-Run command line in child-process, this is just a wrapper for [vim.system](<https://neovim.io/doc/user/lua.html#vim.system()>). The only differences are:
+Run command line in child-process and collect all the output. The difference with `vim.system` API is:
 
-- It provides more friendly line-based `on_stdout` and `on_stderr` callbacks in `opts`.
-- By default `text = true` in `opts`.
+- It pass the `on_exit` method in `opts`.
+- It is by default `text = true` in `opts`.
 
 ```lua
---- @alias commons.SpawnLineProcessor fun(line:string):nil
---- @alias commons.SpawnOpts {on_stdout:commons.SpawnLineProcessor, on_stderr:commons.SpawnLineProcessor, [string]:any}
 --- @alias commons.SpawnOnExit fun(completed:vim.SystemCompleted):nil
-
-function run(cmd:string[], opts:commons.SpawnOpts, on_exit:commons.SpawnOnExit?):vim.SystemObj
+--- @alias commons.SpawnCompleteOpts {on_exit:commons.SpawnOnExit?,[string]:any}
+--- @param cmd string[]
+--- @param opts commons.SpawnCompleteOpts?
+--- @return vim.SystemObj
+M.complete = function(cmd, opts)
 ```
 
 Parameters:
 
-- `cmd`: Exactly the same passing to `vim.system`.
+- `cmd`: Command line in string list, exactly the same passing to `vim.system`.
 - `opts`: Almost the same passing to `vim.system`, except:
 
-  - `on_stdout`: Use line-wise callback that will be invoked when receiving a line from stdout.
-  - `on_stderr`: Use line-wise callback that will be invoked when receiving a line from stderr, they are using the same function signature:
+  - `on_exit`: Callback function that will be invoked when child process exit, with signature:
 
     ```lua
-    function fn_line_processor(line:string):nil
+    function on_exit(completed:vim.SystemCompleted):any
     ```
 
-  - `text`: By default is `true`.
-
-- `on_exit`: Exactly the same passing to `vim.system`.
+    - Parameters:
+      - `completed`: The `vim.SystemCompleted` object.
 
 Returns:
 
 - Returns the `vim.SystemObj` object.
 
-Sync/blocking and async/non-blocking:
+Note:
 
-- To run child process in sync/blocking way, set `on_exit` to `nil`, and invoke `wait()` on the returned object to wait for spawn exit.
-- To run child process in async/non-blocking way, set `on_exit` to lua function and don't invoke `wait()` on the returned object to don't wait.
+If you want to run this API in async mode, pass the `on_exit` function in `opts`, and DO NOT invoke the `wait` method. For example:
 
-### `system`
+```lua
+local job = require("commons.spawn").complete({"cat", "README.md"}, {
+  on_exit = function(completed)
+    print(string.format("exit code:%d", completed.code))
+    print(string.format("signal:%d", completed.signal))
+    print(string.format("process stdout:%d", vim.inspect(completed.stdout)))
+    print(string.format("process stderr:%d", vim.inspect(completed.stderr)))
+  end,
+})
 
-Same function with the [vim.system()](<https://neovim.io/doc/user/lua.html#vim.system()>) since 0.10.
+-- Ignore the returned `job`, don't wait for it, thus this job will run in async.
+```
+
+If you want to run this API in sync mode, don't the pass `on_exit` function in `opts`, and invoke the `wait` method on the returned job. For example:
+
+```lua
+local job = require("commons.spawn").complete({"cat", "README.md"})
+
+-- Wait for the job done, this is sync.
+local completed = job:wait()
+
+print(string.format("exit code:%d", completed.code))
+print(string.format("signal:%d", completed.signal))
+print(string.format("process stdout:%d", vim.inspect(completed.stdout)))
+print(string.format("process stderr:%d", vim.inspect(completed.stderr)))
+```
+
+### `linewise`
+
+Run command line in child-process and process each line of output while running. The difference with `vim.system` API is:
+
+- It process each line in the output of child-process while the process is running.
+- It is by default `text = true` in `opts`.
+
+```lua
+--- @alias commons.SpawnLineWiseProcessor fun(line:string):any
+--- @alias commons.SpawnLineWiseOpts {on_stdout:commons.SpawnLineWiseProcessor,on_stderr:commons.SpawnLineWiseProcessor?,on_exit:commons.SpawnOnExit?,[string]:any}
+--- @param cmd string[]
+--- @param opts commons.SpawnLineWiseOpts?
+--- @return vim.SystemObj
+M.linewise = function(cmd, opts)
+```
+
+Parameters:
+
+- `cmd`: Command line in string list, exactly the same passing to `vim.system`.
+- `opts`: Almost the same passing to `vim.system`, except:
+
+  - `on_stdout`: Callback function that will be invoked on each line of child process output `stdout`, with signature:
+
+    ```lua
+    function on_stdout(line:string):any
+    ```
+
+    - Parameters:
+      - `line`: Each line of the process output `stdout`.
+
+  - `on_stderr`: Callback function that will be invoked on each line of child process output `stderr`, with signature:
+
+    ```lua
+    function on_stderr(line:string):any
+    ```
+
+    - Parameters:
+      - `line`: Each line of the process output `stderr`.
+
+  - `on_exit`: Callback function that will be invoked when child process exit, with signature:
+
+    ```lua
+    function on_exit(completed:vim.SystemCompleted):nil
+    ```
+
+    - Parameters:
+      - `completed`: The `vim.SystemCompleted` object.
+
+Returns:
+
+- Returns the `vim.SystemObj` object.
+
+Note:
+
+If you want to run this API in async mode, pass the `on_exit` function in `opts`, and DO NOT invoke the `wait` method. For example:
+
+```lua
+local job = require("commons.spawn").complete({"cat", "README.md"}, {
+  on_stdout = function(line)
+    print(string.format("processed stdout:%d", vim.inspect(line)))
+  end,
+  on_stderr = function(line)
+    print(string.format("processed stderr:%d", vim.inspect(line)))
+  end,
+  on_exit = function(completed)
+    print(string.format("exit code:%d", completed.code))
+    print(string.format("signal:%d", completed.signal))
+  end,
+})
+
+-- Ignore the returned `job`, don't wait for it, thus this job will run in async.
+```
+
+If you want to run this API in sync mode, don't the pass `on_exit` function in `opts`, and invoke the `wait` method on the returned job. For example:
+
+```lua
+local job = require("commons.spawn").complete({"cat", "README.md"}, {
+  on_stdout = function(line)
+    print(string.format("processed line:%d", vim.inspect(line)))
+  end,
+  on_stderr = function(line)
+    print(string.format("processed stderr:%d", vim.inspect(line)))
+  end,
+})
+
+-- Wait for the job done, this is sync.
+local completed = job:wait()
+
+print(string.format("exit code:%d", completed.code))
+print(string.format("signal:%d", completed.signal))
+```
--- spec/commons/spawn_spec.lua
+++ spec/commons/spawn_spec.lua
@@ -19,9 +19,9 @@ describe("commons.spawn", function()
 
   local dummy = function() end
 
-  describe("[blocking]", function()
+  describe("[wait linewise]", function()
     it("test1", function()
-      local sp = spawn.run({ "cat", "README.md" }, { on_stdout = dummy, on_stderr = dummy })
+      local sp = spawn.linewise({ "cat", "README.md" }, { on_stdout = dummy, on_stderr = dummy })
       sp:wait()
       -- print(string.format("spawn wait-1:%s\n", vim.inspect(sp)))
     end)
@@ -35,7 +35,10 @@ describe("commons.spawn", function()
         assert_eq(line, lines[i])
         i = i + 1
       end
-      local sp = spawn.run({ "cat", "README.md" }, { on_stdout = process_line, on_stderr = dummy })
+      local sp = spawn.linewise(
+        { "cat", "README.md" },
+        { on_stdout = process_line, on_stderr = dummy }
+      )
       sp:wait()
       -- print(string.format("spawn wait-2:%s\n", vim.inspect(sp)))
     end)
@@ -53,7 +56,7 @@ describe("commons.spawn", function()
           assert_eq(line, lines[i])
           i = i + 1
         end
-        local sp = spawn.run(
+        local sp = spawn.linewise(
           { "cat", "README.md" },
           { on_stdout = process_line, on_stderr = dummy }
         )
@@ -78,7 +81,7 @@ describe("commons.spawn", function()
           assert_eq(line, lines[i])
           i = i + 1
         end
-        local sp = spawn.run(
+        local sp = spawn.linewise(
           { "cat", "README.md" },
           { on_stdout = process_line, on_stderr = dummy }
         )
@@ -94,7 +97,7 @@ describe("commons.spawn", function()
       delimiter_i = delimiter_i + math.random(1, 5)
     end
     it("stderr", function()
-      local sp = spawn.run({ "cat", "README.md" }, { on_stdout = dummy, on_stderr = dummy })
+      local sp = spawn.linewise({ "cat", "README.md" }, { on_stdout = dummy, on_stderr = dummy })
       sp:wait()
       -- print(string.format("spawn wait-3:%s\n", vim.inspect(sp)))
     end)
@@ -103,20 +106,19 @@ describe("commons.spawn", function()
       local function process_line(line)
         -- print(string.format("process[%d]:%s\n", i, line))
       end
-      local sp = spawn.run(
+      local sp = spawn.linewise(
         { "cat", "non_exists.txt" },
-        { on_stdout = process_line, on_stderr = process_line, blocking = true }
+        { on_stdout = process_line, on_stderr = process_line }
       )
       sp:wait()
       -- print(string.format("spawn wait-4:%s\n", vim.inspect(sp)))
     end)
   end)
-  describe("[nonblocking]", function()
+  describe("[no-wait linewise]", function()
     it("open", function()
-      local sp = spawn.run(
+      local sp = spawn.linewise(
         { "cat", "README.md" },
-        { on_stdout = dummy, on_stderr = dummy },
-        function(completed) end
+        { on_stdout = dummy, on_stderr = dummy, on_exit = function(completed) end }
       )
       sp:kill(9)
       -- print(string.format("spawn nonblocking-1:%s\n", vim.inspect(sp)))
@@ -131,10 +133,9 @@ describe("commons.spawn", function()
         assert_eq(line, lines[i])
         i = i + 1
       end
-      local sp = spawn.run(
+      local sp = spawn.linewise(
         { "cat", "README.md" },
-        { on_stdout = process_line, on_stderr = dummy },
-        function(completed) end
+        { on_stdout = process_line, on_stderr = dummy, on_exit = function(completed) end }
       )
       -- print(string.format("spawn nonblocking-2:%s\n", vim.inspect(sp)))
     end)
@@ -148,11 +149,107 @@ describe("commons.spawn", function()
         assert_eq(line, lines[i])
         i = i + 1
       end
-      local sp = spawn.run(
+      local sp = spawn.linewise(
         { "cat", "README.md" },
-        { on_stdout = process_line, on_stderr = dummy },
-        function(completed) end
+        { on_stdout = process_line, on_stderr = dummy, on_exit = function(completed) end }
       )
+      -- print(string.format("spawn nonblocking-3:%s\n", vim.inspect(sp)))
+    end)
+  end)
+
+  describe("[wait complete]", function()
+    it("test1", function()
+      local sp = spawn.complete({ "cat", "README.md" })
+      local completed = sp:wait()
+      print(string.format("spawn wait-1:%s\n", vim.inspect(completed)))
+    end)
+    it("test2", function()
+      local lines = fio.readlines("README.md") --[[@as table]]
+
+      local sp = spawn.complete({ "cat", "README.md" }, {})
+      local completed = sp:wait()
+      print(string.format("spawn wait-2:%s\n", vim.inspect(completed)))
+    end)
+    local delimiter_i = 0
+    while delimiter_i <= 25 do
+      -- lower case: a
+      local lower_char = string.char(97 + delimiter_i)
+      it(string.format("stdout on %s", lower_char), function()
+        local lines = fio.readlines("README.md") --[[@as table]]
+
+        local i = 1
+        local function process_line(line)
+          -- print(string.format("[%d]%s\n", i, line))
+          assert_eq(type(line), "string")
+          assert_eq(line, lines[i])
+          i = i + 1
+        end
+        local sp = spawn.complete({ "cat", "README.md" })
+        local completed
+        sp:wait()
+        print(
+          string.format(
+            "spawn wait-delimiter-%d:%s\n",
+            vim.inspect(delimiter_i),
+            vim.inspect(completed)
+          )
+        )
+      end)
+      -- upper case: A
+      local upper_char = string.char(65 + delimiter_i)
+      it(string.format("stdout on %s", upper_char), function()
+        local lines = fio.readlines("README.md") --[[@as table]]
+
+        local i = 1
+        local function process_line(line)
+          -- print(string.format("[%d]%s\n", i, line))
+          assert_eq(type(line), "string")
+          assert_eq(line, lines[i])
+          i = i + 1
+        end
+        local sp = spawn.complete({ "cat", "README.md" }, {})
+        local completed = sp:wait()
+        print(
+          string.format(
+            "spawn wait-uppercase-%d:%s\n",
+            vim.inspect(delimiter_i),
+            vim.inspect(completed)
+          )
+        )
+      end)
+      delimiter_i = delimiter_i + math.random(1, 5)
+    end
+  end)
+  describe("[no-wait complete]", function()
+    it("open", function()
+      local sp = spawn.complete({ "cat", "README.md" }, { on_exit = function(completed) end })
+      sp:kill(9)
+      -- print(string.format("spawn nonblocking-1:%s\n", vim.inspect(sp)))
+    end)
+    it("consume line", function()
+      local lines = fio.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(line, lines[i])
+        i = i + 1
+      end
+      local sp = spawn.complete({ "cat", "README.md" }, { on_exit = function(completed) end })
+      -- print(string.format("spawn nonblocking-2:%s\n", vim.inspect(sp)))
+    end)
+    it("stdout on newline", function()
+      local lines = fio.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s\n", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(line, lines[i])
+        i = i + 1
+      end
+      local sp = spawn.complete({ "cat", "README.md" }, { on_exit = function(completed) end })
       -- print(string.format("spawn nonblocking-3:%s\n", vim.inspect(sp)))
     end)
   end)
