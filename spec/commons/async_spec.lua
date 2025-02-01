ab63b623d0a1b97d6f3eba5f2778a74813584760
linrongbin16 <linrongbin16@outlook.com>
2025-02-01 17:50:36 +0800

    Update async_spec.lua

--- spec/commons/async_spec.lua
+++ spec/commons/async_spec.lua
@@ -21,27 +21,27 @@ describe("commons.async", function()
       local system = async.wrap(vim.system --[[@as function]], 3)
 
       async.void(function()
-        local stdout = ""
+        local stdout_data = ""
         local completed1 = system({ "echo", "foo" }, {
           text = true,
           stdout = function(err, data)
-            stdout = stdout .. data
+            stdout_data = stdout_data .. data
           end,
         })
         print(string.format("async spawn-1 completed:%s", vim.inspect(completed1)))
-        print(string.format("async spawn-1 stdout:%s", vim.inspect(stdout)))
+        print(string.format("async spawn-1 stdout:%s", vim.inspect(stdout_data)))
       end)()
 
       async.void(function()
-        local stdout = ""
+        local stdout_data = ""
         local completed2 = system({ "echo", "foo" }, {
           text = true,
           stdout = function(err, data)
-            stdout = stdout .. data
+            stdout_data = stdout_data .. data
           end,
         })
         print(string.format("async spawn-2 completed:%s", vim.inspect(completed2)))
-        print(string.format("async spawn-2 stdout:%s", vim.inspect(stdout)))
+        print(string.format("async spawn-2 stdout:%s", vim.inspect(stdout_data)))
       end)()
     end)
   end)
